ecca30521ca0af6761ad09dd22cf4b6339f6835a
kwjang <kwanghyun.jang@gmail.com>
2016-02-11 23:15:51 -0800

    Consul integration working code

--- NodeJS/Oauth2ClientApp/app.js
+++ NodeJS/Oauth2ClientApp/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var consul = require('consul')({host:'10.106.8.158', port:'8500'});
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var oauth = require('./routes/oauth');
@@ -62,3 +64,44 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 console.log("App is running.........");
+
+// Returns the members as seen by the consul agent.
+consul.agent.members(function(err, result) {
+  if (err) throw err;
+  console.log("[consul.agent.members] DONE");
+  console.log(result);
+});
+
+// Registers a new service.
+consul.agent.service.register({name : 'node-demo-app', id : 'node-demo-app-12345'}, function(err) {
+  if (err) throw err;
+  console.log("[consul.agent.service.register] DONE");
+});
+
+
+// Registers a new check.
+
+// var check = {
+//   name: 'node-demo-app-check',
+//   ttl: '10s',
+//   notes: 'This is an node-demo-app check.',
+// };
+
+// consul.agent.check.register(check, function(err) {
+//   if (err) throw err;
+//   console.log("[consul.agent.check.register] DONE");
+// });
+
+
+consul.agent.check.deregister('node-demo-app', function(err) {
+  if (err) throw err;
+  console.log("[consul.agent.check.deregister] DONE");
+});
+
+// Returns the nodes and health info of a service.
+consul.health.service({service:'spark', passing : true }, function(err, result) {
+  if (err) throw err;
+  console.log("[consul.health.service] DONE");
+  console.log(result);
+});
+
